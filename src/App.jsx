5634e59b1084b1253fe1123d0fb42acfd15f5234
M2013030 <christosp1990@gmail.com>
2022-04-26 22:02:46 +0200

    Better conditional rendering for confirmation page

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
-import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 import React from "react";
-import Form from "./components/form.component";
 import DashboardLogin from "./pages/administration";
 import Confirmation from "./pages/confirmation";
 import Dashboard from "./pages/dashboard";
 import Homapage from "./pages/homepage";
 
-const ConfirmationWrapper = ({ children, employee }) => {
-  return employee ? children : <Navigate to="/" replace />;
-};
-
 function App() {
-  const employee = useSelector(
-    (state) => state.employeeToRegister.newEmployee.firstName
-  );
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Homapage />} />
         <Route path="/administration" element={<DashboardLogin />} />
         <Route path="/administration/dashboard" element={<Dashboard />} />
-        <Route
-          path="/confirmation"
-          element={
-            <ConfirmationWrapper employee={employee}>
-              <Confirmation />
-            </ConfirmationWrapper>
-          }
-        />
+        <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
     </>
   );
--- src/pages/confirmation.jsx
+++ src/pages/confirmation.jsx
@@ -3,11 +3,21 @@ import { motion, AnimatePresence } from "framer-motion";
 import ConfirmationComp from "../components/confirmation.component";
 import { useSelector } from "react-redux";
 import ErrorMessage from "../components/errorMessage";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Confirmation() {
 
   const status = useSelector(state => state.employeeToRegister.statusCode)
-
+  const employee = useSelector(state => state.employeeToRegister.newEmployee)
+  console.log(employee)
+  const navigate = useNavigate()
+  useEffect(() => {
+    Object.keys(employee).length === 0 ?
+      navigate('/', { replace: true })
+      : null
+  },
+    [employee]);
   return (
     <AnimatePresence>
       <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1.5 }}>
